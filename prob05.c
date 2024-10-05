8b664a8b1e752631d99d17cd31f8de5987dae335
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-04 23:22:56 -0500

    Se intento resolver el ejercicio 05, usando la funcion reverse a toda la fila, pero se encontro el error de que tambien se cambiaban de color los Square

---
+++ prob05.c
@@ -0,0 +1,11 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** blackSquare = reverse(whiteSquare);
+  char** joinFigures = join(blackSquare, whiteSquare);
+  char** repeatFigures = repeatH(joinFigures, 4);
+  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
+  char** reverseCombineRow = reverse(combineRow);
+  interpreter(reverseCombineRow);
+}
