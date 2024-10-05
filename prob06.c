c04f46d45eb93c6e662051fa0806893ca7948243
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2024-10-04 23:48:38 -0500

    Se probo agregar los peones a la delantera de las fichas principales, conociendo previamente el funcionamiento de superImpose se dio los parametros la repeticion de los peones mediante la funcion repeatH y el parametro de la variable que corresponde a la fila, donde los peones seran ubicados.

---
+++ prob06.c
@@ -0,0 +1,12 @@
+#include "chess.h"
+#include "figures.h"
+
+void display(){
+  char** blackSquare = reverse(whiteSquare);
+  char** joinFigures = join(blackSquare, whiteSquare);
+  char** repeatFigures = repeatH(joinFigures, 4);
+  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
+  char** reverseCombineRow = combineRow;
+  char** whitePlace = up(reverseCombineRow, superImpose(repeatH(pawn, 8), reverse(repeatFigures)));
+  interpreter(whitePlace);
+}
