c20d861b0f906c925a9c3180969db4a85901bfe4
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2024-10-04 23:58:37 -0500

    Se realizo prueba para intentar replicar la figura solicitada en el problema 6, aunque el resultado no fue exitoso se acerco relativamente.

--- prob06.c
+++ prob06.c
@@ -8,5 +8,9 @@ void display(){
   char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
   char** reverseCombineRow = combineRow;
   char** whitePlace = up(reverseCombineRow, superImpose(repeatH(pawn, 8), reverse(repeatFigures)));
-  interpreter(whitePlace);
+  char** emptyPlace = repeatFigures;  
+  for(int i = 0; i < 3; i++){
+    emptyPlace = up(emptyPlace, reverse(emptyPlace));
+  }
+  interpreter(up(whitePlace, emptyPlace));
 }
