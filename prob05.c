fa64f252ae40ca1b248c0d05392acc525eb367f3
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-04 23:25:59 -0500

    Se uso la funcion reverse en la parte de repeticion de los Square, para que cuando se vuelva a usar un reverse no haya cambios en este, se obtuvo un resultado exitoso cumpliendo los requisitos del ejercicio

--- prob05.c
+++ prob05.c
@@ -5,7 +5,7 @@ void display(){
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
-  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
+  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), reverse(repeatFigures));
   char** reverseCombineRow = reverse(combineRow);
   interpreter(reverseCombineRow);
 }
