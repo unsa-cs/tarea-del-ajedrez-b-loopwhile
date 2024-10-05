79c074b5d3a696e1bbb126a95c0e49382d4573e9
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-04 23:18:39 -0500

    Se completo los requisitos del ejercicio 4, se uso una serie de joins y por ultimo un superImpose, se consiguio un resultado exitoso

--- prob04.c
+++ prob04.c
@@ -5,6 +5,6 @@ void display(){
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
-  char** combineRow = superImpose(join(join(join(rook, knight), join(bishop, queen)), join((king, bishop), join(knight, rook))), repeatFigures);
+  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
   interpreter(combineRow);
 }
