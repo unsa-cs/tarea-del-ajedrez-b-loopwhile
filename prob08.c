0c3a0ad4359a57da2ef6efc75686f5d323618ec0
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2024-10-06 16:58:38 -0500

    A partir de la 'unidad elementar de qudrante 2x2' resulto mas sencillo armar un tablero con la solucion al problema de las 8 reinas empleando esencialmente funcion up y join.

--- prob08.c
+++ prob08.c
@@ -12,5 +12,11 @@ void display(){
   char** quadrantType2 = up(join(reverse(whiteSquare), queenWhiteSquare), reverse(twoSquares));
   char** quadrantType3 = up(twoSquares, join(queenWhiteSquare, reverse(whiteSquare)));
   char** quadrantType4 = up(twoSquares, join(whiteSquare, queenBlackSquare));
-  interpreter(quadrantType4);
+
+  char** board = join(join(join(quadrantType0, quadrantType2), quadrantType0), quadrantType3);
+  board = up(board, join(join(join(quadrantType0, quadrantType1), quadrantType0), quadrantType4));
+  board = up(board, join(join(join(quadrantType2, quadrantType0), quadrantType3), quadrantType0));
+  board = up(board, join(join(join(quadrantType1, quadrantType0), quadrantType4), quadrantType0));
+
+  interpreter(board);
 }
