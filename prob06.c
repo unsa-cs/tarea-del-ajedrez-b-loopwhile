9ba40ed67c93e51cf82638e320dbe1f672b377e5
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-05 20:54:54 -0500

    Se esta modificando el codigo para que este de manera ordenado creando variables para cada color de figuras, de esta manera lograr reutilizar codigo

--- prob06.c
+++ prob06.c
@@ -5,7 +5,10 @@ void display(){
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
-  char** combineRow = superImpose(join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook), repeatFigures);
+  char** figuresPlace = up(repeatFigures, reverse(repeatFigures);
+  char** combineRow = join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook);
+  char** combinePawn = repeatH(pawn, 8);
+  char** imposeCombineRow = superImpose(combineRow, repeatFigures);
   char** reverseCombineRow = combineRow;
   char** whitePlace = up(reverseCombineRow, superImpose(repeatH(pawn, 8), reverse(repeatFigures)));
   char** emptyPlace = repeatFigures;  
