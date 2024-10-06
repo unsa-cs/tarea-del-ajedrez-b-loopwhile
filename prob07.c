045c13c14b4f04361f1c03538102b69243d41e2e
Gerardo <gerardo_colque13@hotmail.com>
2024-10-06 16:08:54 -0500

    Resolvi el problema 7 corrigiendo algunos errores de tipeo y logica, cumpli con los requisitos del ejercicio

--- prob07.c
+++ prob07.c
@@ -5,6 +5,7 @@ void display(){
   
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
+  char** rjoinFigures = reverse(joinFigures);
   char** repeatFigures = repeatH(joinFigures, 4);
   char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
   char** boardFinal = repeatV(figuresPlace, 3);
@@ -13,12 +14,12 @@ void display(){
   char** knight2 = rotateL(knight1);
   char** knight3 = rotateL(knight2);
   char** knight4 = rotateL(knight3);
-  char** joinKnights1 = join(join(join(join(knight1, joinFigures), joinFigures), joinFigures), knight2);
-  char** joinKnights2 = join(join(join(join(knight3, joinFigures), joinFigures), joinFigures), knight4);
+  char** joinKnights1 = join(join(join(join(superImpose(knight1,whiteSquare), joinFigures), joinFigures), joinFigures), superImpose(knight2,blackSquare));
+  char** joinKnights2 = join(join(join(join(superImpose(knight3, blackSquare), rjoinFigures), rjoinFigures), rjoinFigures), superImpose(knight4, whiteSquare));
 
   char** final = superImpose(joinKnights1,repeatFigures);
   char** unir1 = up(final, boardFinal);
   char** final2 = superImpose(joinKnights2, repeatFigures);
-  char** tablero = up(unir1,reverse(final2));
+  char** tablero = up(unir1,final2);
   interpreter(tablero);
 }
