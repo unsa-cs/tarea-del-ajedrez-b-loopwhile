4c1479dbbdf3e10b6ec17d3fc7575383d4bbf421
kimyork32 <CaronteGG.20851@gmail.com>
2024-10-05 21:34:08 -0500

    cree cuatro variables para cada knight y asi rotarlos usando las funciones rotateV y rotateH, los junté con la funcion join y los mostré)

--- prob07.c
+++ prob07.c
@@ -7,7 +7,10 @@ void display(){
   char** repeatFigures = repeatH(joinFigures, 4);
   char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
   char** boardFinal = repeatV(figuresPlace,4);
-  char** knight1 = knight;
-  knight1 = rotateL(knight);
-  interpreter(knight1);
+  char** knight1 = rotateL(knight);
+  char** knight2 = rotateR(knight);
+  char** knight3 = rotateL(knight1);
+  char** knight4 = rotateL(knight2);
+  char** joinKnights = join(join(join(knight1, knight2), knight2), knight4);
+  interpreter(joinKnights);
 }
