7e3670c605735c30f325f398bfd02f606d3840ee
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-05 21:02:56 -0500

    Intente modificar el codigo para que todo este de manera ordenara, se encontro un error malloc(): corrupted top size.

--- prob06.c
+++ prob06.c
@@ -5,16 +5,12 @@ void display(){
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
-  char** figuresPlace = up(repeatFigures, reverse(repeatFigures);
+  char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
   char** combineRow = join(join(join(join(join(join(join(rook, knight), bishop), queen), king), bishop), knight), rook);
   char** combinePawn = repeatH(pawn, 8);
-  char** imposeCombineRow = superImpose(combineRow, repeatFigures);
-  char** reverseCombineRow = combineRow;
-  char** whitePlace = up(reverseCombineRow, superImpose(repeatH(pawn, 8), reverse(repeatFigures)));
-  char** emptyPlace = repeatFigures;  
-  emptyPlace = repeatV(up(emptyPlace, reverse(emptyPlace)), 2);
-  char** boardFinal = up(whitePlace, emptyPlace);
-  char** blackPlace = whitePlace;
-  boardFinal = up(boardFinal, flipH(reverse(blackPlace)));
+  char** whitePlace = superImpose(up(combineRow, combinePawn), figuresPlace);
+  char** blackPlace = superImpose(reverse(up(combinePawn,combineRow)), figuresPlace);
+  char** emptyPlace = repeatH(figuresPlace,2);
+  char** boardFinal = up(up(whitePlace, emptyPlace), blackPlace);
   interpreter(boardFinal);
 }
