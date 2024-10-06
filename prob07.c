64d5d7639c423f4336b66a5219a06155050a0415
ritovi <149262897+ritovi@users.noreply.github.com>
2024-10-05 23:12:40 -0400

    se ha utilizado la funcion rotateL para obtener una rotacion distinta de cada knight, el primero es una rotacion 0, el segundo una rotacion 90L, el tercero es una rotacion 90L, y el ultimo otra rotacion 90L, y se obtuvo 4 knights con una posicion distinta

--- prob07.c
+++ prob07.c
@@ -2,15 +2,17 @@
 #include "figures.h"
 
 void display(){
+  
   char** blackSquare = reverse(whiteSquare);
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
   char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
   char** boardFinal = repeatV(figuresPlace,4);
-  char** knight1 = rotateL(knight);
-  char** knight2 = rotateR(knight);
-  char** knight3 = rotateL(knight1);
-  char** knight4 = rotateL(knight2);
-  char** joinKnights = join(join(join(knight1, knight2), knight2), knight4);
+  
+  char** knight1 =knight;
+  char** knight2 = rotateL(knight1);
+  char** knight3 = rotateL(knight2);
+  char** knight4 = rotateL(knight3);
+  char** joinKnights = join(join(join(knight1, knight2), knight3), knight4);
   interpreter(joinKnights);
 }
