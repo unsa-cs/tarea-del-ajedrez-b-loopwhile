f1aace27843520ce18603d7675e3c53e74b06c16
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2024-10-06 16:44:49 -0500

    Se han implementado todos los casos de cuandrantes (2x2) necesarios para formar posteriormente un tablero completo con la solucion de las 8 reinas, se trata de 5 tipos de cuadrantes: 1 cuadrante vacio y 4 cuadrantes con la reina de una posicion diferente.

--- prob08.c
+++ prob08.c
@@ -2,31 +2,15 @@
 #include "figures.h"
 
 void display(){
-  
-  char** blackSquare = reverse(whiteSquare);
-  char** joinFigures = join(blackSquare, whiteSquare);
-  char** rjoinFigures = reverse(joinFigures);
-  char** repeatFigures = repeatH(joinFigures, 4);
-  char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
-  char** boardFinal = repeatV(figuresPlace, 3);
-  
-  char** knight1 =knight;
-  char** knight2 = rotateL(knight1);
-  char** knight3 = rotateL(knight2);
-  char** knight4 = rotateL(knight3);
-  char** joinKnights1 = join(join(join(join(superImpose(knight1,whiteSquare), joinFigures), joinFigures), joinFigures), superImpose(knight2,blackSquare));
-  char** joinKnights2 = join(join(join(join(superImpose(knight3, blackSquare), rjoinFigures), rjoinFigures), rjoinFigures), superImpose(knight4, whiteSquare));
-
-  char** final = superImpose(joinKnights1,repeatFigures);
-  char** unir1 = up(final, boardFinal);
-  char** final2 = superImpose(joinKnights2, repeatFigures);
-  char** tablero = up(unir1,final2);
-  //=========
   char** queenBlackSquare = superImpose(queen, reverse(whiteSquare));
   char** queenWhiteSquare = superImpose(queen, whiteSquare);
 
   char** twoSquares = join(reverse(whiteSquare), whiteSquare);
-
+  
+  char** quadrantType0 = up(twoSquares, reverse(twoSquares));
   char** quadrantType1 = up(join(queenBlackSquare, whiteSquare), reverse(twoSquares));
-  interpreter(quadrantType1);
+  char** quadrantType2 = up(join(reverse(whiteSquare), queenWhiteSquare), reverse(twoSquares));
+  char** quadrantType3 = up(twoSquares, join(queenWhiteSquare, reverse(whiteSquare)));
+  char** quadrantType4 = up(twoSquares, join(whiteSquare, queenBlackSquare));
+  interpreter(quadrantType4);
 }
