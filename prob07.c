4b047910715169b3f1c4b9d5076ad000110b0af2
Gerardo <gerardo_colque13@hotmail.com>
2024-10-05 23:07:07 -0500

    Como prueba quise rellenar el tablero, con los caballos en cada esquina del tablero, pero los colores de los casilleros donde estan mis caballos no son los indicados

--- prob07.c
+++ prob07.c
@@ -7,15 +7,15 @@ void display(){
   char** joinFigures = join(blackSquare, whiteSquare);
   char** repeatFigures = repeatH(joinFigures, 4);
   char** figuresPlace = up(repeatFigures, reverse(repeatFigures));
-  char** boardFinal = repeatV(figuresPlace,4);
+  char** boardFinal = repeatV(figuresPlace, 3);
   
   char** knight1 =knight;
   char** knight2 = rotateL(knight1);
   char** knight3 = rotateL(knight2);
   char** knight4 = rotateL(knight3);
-  char** joinKnights = join(join(join(join(join(knight1, knight2), knight3), knight),joinFigures),joinFigures);
+  char** joinKnights = join(join(join(join(knight1, joinFigures), joinFigures), joinFigures), knight2);
 
   char** final = superImpose(joinKnights,repeatFigures);
-  char** tablero = up(final, reverse(boardFinal));
+  char** tablero = up(final, boardFinal);
   interpreter(tablero);
 }
