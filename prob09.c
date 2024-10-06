bc8d7e96a3ef9848aae4000a6776fad1117c80c7
paulqm27 <pquispemor@unsa.edu.pe>
2024-10-06 17:37:37 -0500

    Se creo un struct que recibe un caracter, fila y columna para poder almacenar las fichas de las jugadas que pide el ejercicio, falta poner las condiciones en los bucles.

--- prob09.c
+++ prob09.c
@@ -2,11 +2,25 @@
 #include "figures.h"
 #include <stdio.h>
 
+struct Piezas{
+  char piece;
+  int fila;
+  int columna;
+};
+
 void display(){
+  struct Piezas piezas[16] = {
+    {'q', 0, 5},
+    {'k', 0, 7},
+    {'t', 1, 3},
+    {'t', 1, 5},
+    {'p', 2, 5}
+  };
   char** blackSquare = reverse(whiteSquare);
   char** squareStart = NULL;
   char** row = NULL;
   char** board = NULL;
+  char** piezaTemp = NULL;
   for(int i=0; i<8; i++){
     if(i%2 == 0){
       squareStart = blackSquare;
